3b39f0c3c05bab4a5c92fab6e32913f9bd8041ba
Aniza <oziina9mohamett@gmail.com>
2025-06-24 21:12:25 +0300

    Add menu Api done#

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-"use strict";
+// "use strict";
 
-const hamburgerBtn = document.getElementById("hamburger-btn");
-const nav = document.querySelector(".navigation");
-const menuIcon = hamburgerBtn.querySelector(".menu-icon");
-const closeIconInside = document.querySelector(".close-icon");
-const navLinks = document.querySelectorAll(".navigation-links");
+// const hamburgerBtn = document.getElementById("hamburger-btn");
+// const nav = document.querySelector(".navigation");
+// const menuIcon = hamburgerBtn.querySelector(".menu-icon");
+// const closeIconInside = document.querySelector(".close-icon");
+// const navLinks = document.querySelectorAll(".navigation-links");
 
-// Open menu
-hamburgerBtn.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  hamburgerBtn.style.display = "none"; // hide menu icon when menu is open
-});
+// // Open menu
+// hamburgerBtn.addEventListener("click", () => {
+//   nav.classList.toggle("active");
+//   hamburgerBtn.style.display = "none"; // hide menu icon when menu is open
+// });
 
-// Close menu via inside cross
-closeIconInside.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  hamburgerBtn.style.display = "block";
-});
+// // Close menu via inside cross
+// closeIconInside.addEventListener("click", () => {
+//   nav.classList.toggle("active");
+//   hamburgerBtn.style.display = "block";
+// });
 
 // // Also close when a nav link is clicked
 // navLinks.forEach((link) =>
